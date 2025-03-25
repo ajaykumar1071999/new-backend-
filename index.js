3698054901cd2f683115da7686110643080281af
ajay kumar <ajay@radiantinfonet.com>
2025-03-25 15:54:40 +0530

    cors issue fix

--- index.js
+++ index.js
@@ -1,49 +1,56 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    
+  })
+);
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Server Started');
+app.get("/", (req, res) => {
+  res.send("Server Started");
 });
 
-app.get('/api', (req, res) => {
-
-    res.json({
-        message: 'Welcome to the API',
-        version: '1.0.0'
-    })
-})
-
-app.get('/api/jokes', (req, res) => {
-    const jokes = [
-        {
-            id: 1,
-            joke: 'Why did the scarecrow win an award? Because he was outstanding in his field.'
-        },
-        {
-            id: 2,
-            joke: 'Why did the scarecrow win an award? Because he was outstanding in his field.'
-        },
-        {
-            id: 3,
-            joke: 'Why did the scarecrow win an award? Because he was outstanding in his field.'
-        },
-        {
-            id: 4,
-            joke: 'Why did the scarecrow win an award? Because he was outstanding in his field.'
-        },
-        {
-            id: 5,
-            joke: 'Why did the scarecrow win an award? Because he was outstanding in his field.'
-        }
-    ];
-
-    res.json(jokes);
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Welcome to the API",
+    version: "1.0.0",
+  });
 });
 
+app.get("/api/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      joke: "Why did the scarecrow win an award? Because he was outstanding in his field.",
+    },
+    {
+      id: 2,
+      joke: "Why did the scarecrow win an award? Because he was outstanding in his field.",
+    },
+    {
+      id: 3,
+      joke: "Why did the scarecrow win an award? Because he was outstanding in his field.",
+    },
+    {
+      id: 4,
+      joke: "Why did the scarecrow win an award? Because he was outstanding in his field.",
+    },
+    {
+      id: 5,
+      joke: "Why did the scarecrow win an award? Because he was outstanding in his field.",
+    },
+  ];
+
+  res.json(jokes);
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
