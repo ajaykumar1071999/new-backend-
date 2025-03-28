f0352b5a695e0dc7045754d67d7fc8b789510643
ajay kumar <ajay@radiantinfonet.com>
2025-03-28 14:30:00 +0530

    user and video schema done

---
+++ src/models/user.model.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    avtar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistoey: [
+      {
+        type: SchemaType.Types.ObjectId,
+        ref: 'Video',
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+export default User;
---
+++ src/models/video.model.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const videoSchema = mongoose.Schema(
+  {
+    videoFile: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: true,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  { timeStamps: true }
+);
+
+export default Video = mongoose.model('Video', videoSchema);
