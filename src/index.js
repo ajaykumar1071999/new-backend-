2bca456fe7b4c795b174c0f78b46b10e4ffd8e82
ajay kumar <ajay@radiantinfonet.com>
2025-04-01 15:44:59 +0530

    multer add and file upload to cloudinary

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import 'dotenv/config';
 import { connectToDB } from './db/index.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import bcrypt from 'bcrypt';
+import uploadToCloudinary from './utils/cloudinary.js';
+import { upload } from './middlewares/fileUpload/index.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,6 +22,42 @@ app.use(express.urlencoded({ extends: true, limit: '20kb' }));
 app.use(express.static('public'));
 
 connectToDB();
+
+app.post('/start', upload.single('avtar_image'), async (req, res) => {
+  const avtar_image = req.file;
+  if (!avtar_image) {
+    return res
+      .status(404)
+      .send({ statusCode: 404, message: 'Please send file' });
+  }
+  const image = await uploadToCloudinary(avtar_image);
+  if (!image) {
+    return res
+      .status(500)
+      .send({ statusCode: 500, message: 'Something issue while file upload' });
+  }
+  const { url, format, created_at, original_filename } = image;
+  res.status(200).send({
+    statusCode: 200,
+    message: 'Server is running ',
+    data: { avtar_image: { url, format, created_at, original_filename } },
+  });
+});
+
+async function createPasswordHash(password) {
+  try {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    console.log('Hashed Password:', hashedPassword);
+    return hashedPassword;
+  } catch (error) {
+    console.error('Error hashing password:', error);
+  }
+}
+
+// Example usage
+const password = 'mySecurePassword123';
+createPasswordHash(password);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+
 const userSchema = new mongoose.Schema(
   {
     username: {
@@ -47,3 +48,4 @@ const userSchema = new mongoose.Schema(
 
 const User = mongoose.model('User', userSchema);
 export default User;
+
